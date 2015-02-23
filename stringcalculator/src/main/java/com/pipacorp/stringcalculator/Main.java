fd2a467acf190dcfb3b5561251287ed6f52b1db7
Jugis <vargapalmark92@gmail.com>
2015-02-23 16:40:06 +0100

    első vezió.. Sajnos idő hiányában, nem fogom túl midnen részletre kimenőleg megcsinálni, feltöltöm 2 darabban.

---
+++ stringcalculator/src/main/java/com/pipacorp/stringcalculator/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pipacorp.stringcalculator;
+
+import com.sun.org.apache.bcel.internal.generic.GOTO;
+
+/**
+ *
+ * @author Pali
+ */
+public class Main {
+    public static void main(String... args){
+        String as = "00";
+        metodus(as);
+    }
+
+    private static void metodus(String as) {
+    }
+}
+   
